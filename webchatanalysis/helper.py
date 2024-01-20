df68e03dd17adf6f0ae5b015d1e5f51ea50c0e05
Anurag6883 <137706532+Anurag6883@users.noreply.github.com>
2024-01-20 21:20:38 +0530

    Update helper.py

--- webchatanalysis/helper.py
+++ webchatanalysis/helper.py
@@ -41,8 +41,7 @@ def most_busy_users(df):
     return x,df
 
 
-def create_wordcloud(selected_user,df):
-
+def create_wordcloud(selected_user, df):
     f = open('stop_hinglish.txt', 'r')
     stop_words = f.read()
 
@@ -52,21 +51,18 @@ def create_wordcloud(selected_user,df):
     temp = df[df['user'] != 'group_notification']
     temp = temp[temp['message'] != '<Media omitted>\n']
 
-    def remove_stop_words(message):
-        y = []
-        for word in message.lower().split():
-            if word not in stop_words:
-                y.append(word)
-        return " ".join(y)
+    if not temp['message'].empty:
+        wc = WordCloud(width=500, height=500, min_font_size=10, background_color='white',font_path='/C:/WINDOWS/FONTS')
+        temp['message'] = temp['message'].apply(remove_stop_words)
+        df_wc = wc.generate(temp['message'].str.cat(sep=" "))
+    else:
+        st.warning("No text available for WordCloud generation.")
+        df_wc = None  # You may want to handle this case appropriately
 
-    wc = WordCloud(width=500,height=500,min_font_size=10,background_color='white')
-    temp['message'] = temp['message'].apply(remove_stop_words)
-    df_wc = wc.generate(temp['message'].str.cat(sep=" "))
     return df_wc
     
-def most_common_words(selected_user,df):
-
-    f = open('stop_hinglish.txt','r')
+def most_common_words(selected_user, df, top_n=20):
+    f = open('stop_hinglish.txt', 'r')
     stop_words = f.read()
 
     if selected_user != 'Overall':
@@ -82,9 +78,8 @@ def most_common_words(selected_user,df):
             if word not in stop_words:
                 words.append(word)
 
-    most_common_df = pd.DataFrame(Counter(words).most_common(20))
+    most_common_df = pd.DataFrame(Counter(words).most_common(top_n))
     return most_common_df
-
 # def emoji_helper(selected_user,df):
 #     if selected_user != 'Overall':
 #         df = df[df['user'] == selected_user]
