51c08582df693c4abee63da25dbcdbeed464aae2
Anurag6883 <137706532+Anurag6883@users.noreply.github.com>
2024-01-21 13:03:12 +0530

    Update app.py

--- webchatanalysis/app.py
+++ webchatanalysis/app.py
@@ -156,6 +156,13 @@ if uploaded_file is not None:
             with col2:
                 st.dataframe(new_df)
 
+        # WordCloud
+        st.title("Wordcloud")
+        df_wc = helper.create_wordcloud(selected_user, df)
+        fig, ax = plt.subplots(figsize=(10, 6))
+        ax.imshow(df_wc, interpolation='bilinear')
+        st.pyplot(fig)
+
        
         # Most common words
         most_common_df = helper.most_common_words(selected_user, df)
@@ -164,20 +171,6 @@ if uploaded_file is not None:
         plt.xticks(rotation='horizontal')
         st.title('Most Common Words')
         st.pyplot(fig)
-
-        st.title("Word Cloud")
-
-        # Combine all messages into a single string
-        all_messages = " ".join(df[df['user'] == selected_user]['message'].values)
-
-        # Generate the word cloud
-        wordcloud = WordCloud(width=800, height=400, background_color="white").generate(all_messages)
-
-        # Display the word cloud using matplotlib
-        plt.figure(figsize=(10, 5))
-        plt.imshow(wordcloud, interpolation="bilinear")
-        plt.axis("off")
-        st.pyplot(plt)
 
         # Emoji analysis
         emoji_df = helper.emoji_helper(selected_user, df)
